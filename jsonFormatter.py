3af7f4457c567b6435222ef6d7eaa411496b9643
GoodGuyGregory <greg.witt625@gmail.com>
2021-12-06 13:52:53 -0500

    adds json formatter

--- jsonFormatter.py
+++ jsonFormatter.py
@@ -10,8 +10,8 @@ def formatJson():
     filetoParse = input('Enter the file name to be formatted: ')
 
     fileRegex = re.compile(r'%s' % filetoParse)
-    keyRegex = re.compile(r'(\w+:)')
-    valueRegex = re.compile(r'\(\'.+\'\)')
+    valueSpecificRegex = re.compile(r'\D+')
+    keyValueRegex = re.compile(r'(\w+:) (.+)')
 
     jsonFilesFound = []
 # file json files
@@ -21,6 +21,9 @@ def formatJson():
             print('Found File ' + str(parseableFile[0]) + '... ')
             jsonFilesFound.append(parseableFile[0])
 
+    if len(jsonFilesFound) == 0:
+        print('No files found for %s' % filetoParse)
+
     for jsonfile in jsonFilesFound:
         # open existing json
         openedOriginaljsonFile = open(jsonfile, 'r')
@@ -28,10 +31,32 @@ def formatJson():
         # open new json with formats
         newFormattedFile = open('formated.json', 'w')
 
+#
         for line in originalJson:
-            foundKey = keyRegex.match(line)
+            # search the files for keys
+            foundKey = keyValueRegex.findall(line)
             if foundKey:
-                print('found ' + foundKey[0])
+                # add the found key to the formatted file
+                keyvalue = foundKey[0][0]
+                pairValue = foundKey[0][1][:-1]
+                pairValueSearch = valueSpecificRegex.search(pairValue)
+                if pairValueSearch:
+                    # format the value found to JSON
+                    # reassign the value
+                    pairValue = foundKey[0][1][1:-2]
+                    # slice off the : and append the "<key>" formatting
+                    formattedKeyValuepair = ' \"' + \
+                        keyvalue[0:-1] + '\"' + ':' + \
+                        '\"' + pairValue + '\"' + ','
+                    newFormattedFile.write(formattedKeyValuepair)
+                else:
+                    # the value was a number or not in the regex params
+                    formattedKey = ' \"' + \
+                        keyvalue[0:-1] + '\"' + ':' + pairValue + ','
+                    newFormattedFile.write(formattedKey)
+            # append to the new jsonfile because the key might be alright
+            else:
+                newFormattedFile.write(line)
 
 
 def main():
